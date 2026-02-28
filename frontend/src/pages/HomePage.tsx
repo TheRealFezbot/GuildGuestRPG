8ae03c3e2b206459ffdafc4dbe3d518a20e6e124
Ferry <FerryRietveld@live.nl>
2026-02-28 15:32:48 +0200

    Added logout button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,19 @@
+import { useAuth } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
+
 function HomePage() {
+    const { logout } = useAuth()
+    const navigate = useNavigate()
+
+    const handleLogout = async () => {
+            logout()
+            navigate("/login")
+        }
+    
     return (
         <div>
             <h1>HOMEPAGE</h1>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     )
 }
